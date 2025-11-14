3bdfb710e675824cdd166152f00470926f3a0366
adarshtmg2060 <adarshtmg2060@gmail.com>
2025-11-14 19:08:57 +0545

    all done final

--- server/configs/ai.js
+++ server/configs/ai.js
@@ -2,8 +2,6 @@ import OpenAI from "openai";
 import { config } from "./app.config.js";
 
 const ai = new OpenAI({
-  // apiKey: "AIzaSyCqT0GEwGQvuvMh4McbVVg691myIOReHz0",
-  // baseURL: "https://generativelanguage.googleapis.com/v1beta/openai/",
   apiKey: config.OPENAI_API_KEY,
   baseURL: config.OPENAI_BASE_URL,
 });
--- server/controllers/resumeController.js
+++ server/controllers/resumeController.js
@@ -2,14 +2,6 @@ import Resume from "../models/Resume.js";
 import fs from "fs";
 import imageKit from "../configs/imageKit.js";
 
-// import { ImageKit } from "@imagekit/nodejs";
-
-// const imageKit = new ImageKit({
-//   publicKey: "public_mCeMHEZMEQTAHUhEQy74wc/tWDk=",
-//   privateKey: "private_e4TEYWR7z8NTADUsoeteLRJo7EY=",
-//   urlEndpoint: "https://ik.imagekit.io/x7xxa304at", // Required
-// });
-
 // controller for creating a resume
 // POST: /api/resumes/create
 export const createResume = async (req, res) => {
